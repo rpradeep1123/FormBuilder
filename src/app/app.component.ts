8bb6fab72839ae92923a51b8d3178edefe34977f
Pradeep <rpradeep3696@gmail.com>
2020-07-23 21:34:02 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormBuilder';
+  @ViewChild('json') jsonElement?: ElementRef;
+  public form: Object = { components: [] };  
 }
