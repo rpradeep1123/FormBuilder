802721187036f9de8dcc8363e887b142913fe8ee
Pradeep <rpradeep3696@gmail.com>
2020-07-30 15:18:54 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,68 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { config, defaultI18n, defaultOptions } from "./formbuilder/config";
+import { FormBuilderCreateor } from "./formbuilder/form-builder";
+import I18N from "./formbuilder/mi18n";
 
+declare var jQuery;
+
+function initJq() {
+  (function ($) {
+    (<any>$.fn).formBuilder = function (options) {
+      if (!options) {
+        options = {};
+      }
+      let elems = this;
+      let {i18n, ...opts} = $.extend({}, defaultOptions, options, true);
+      (<any>config).opts = opts;
+      let i18nOpts = $.extend({}, defaultI18n, i18n, true);
+      let instance = {
+        actions: {
+          getData: null,
+          setData: null,
+          save: null,
+          showData: null,
+          setLang: null,
+          addField: null,
+          removeField: null,
+          clearFields: null
+        },
+        get formData() {
+          return instance.actions.getData('json');
+        },
+
+        promise: new Promise(function (resolve, reject) {
+          new I18N().init(i18nOpts).then(() => {
+            elems.each(i => {
+              let formBuilder = new FormBuilderCreateor().getFormBuilder(opts, elems[i]);
+              $(elems[i]).data('formBuilder', formBuilder);
+              instance.actions = formBuilder.actions;
+            });
+            delete instance.promise;
+            resolve(instance);
+          }).catch(console.error);
+        })
+
+      };
+
+      return instance;
+    };
+  })(jQuery);
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'FormBuilder';
   @ViewChild('json') jsonElement?: ElementRef;
   public form: Object = { components: [] };  
+  formBuilder: any;
+
+  ngOnInit(): void {
+    initJq();
+    this.formBuilder = (<any>jQuery('.build-wrap')).formBuilder();
+    console.log(this.formBuilder);
+  }
+
 }
